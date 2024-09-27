1a93a5830fae7d0ae3ede56f6282410b1d8e7ee6
Herge7 <zolasergedesire@gmail.com>
2024-09-27 17:38:13 +0100

    Reviewing the code in commentaries

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -1,14 +1,13 @@
 
 
 
-
+"Reviewing code"
 class Item:
     def __init__(self,prix, weight):
         self.price = prix     
         self.weight = weight  
 
     def display_item(self):
-       
         print(f"Item: Price: {self.price}, Weight: {self.weight} kg")
 
 item1 = Item( 45, 10)  
