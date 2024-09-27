22d832dc418fe84b2bd8a6e8432aee9797919362
Herge7 <zolasergedesire@gmail.com>
2024-09-27 07:55:20 +0100

    Displaying class item
    .

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -1,11 +1,15 @@
-import unittest
-
-from exo1 import Item
 
 
-class Exo1Test(unittest.TestCase):
 
-    def test_item_construction(self):
-        item = Item(10, 20)
 
-        self.assertEqual(20, item.weight)
+class Item:
+    def __init__(self,price, weight):
+        self.price = price      
+        self.weight = weight  
+
+    def display_item(self):
+       
+        print(f"Item: Price: {self.price}, Weight: {self.weight} kg")
+
+item1 = Item( 0.5, 10)  
+item1.display_item()                
