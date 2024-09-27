028a87bc32c3b79b03ad8977459956882d32b3f2
Herge7 <zolasergedesire@gmail.com>
2024-09-27 08:04:29 +0100

    Displaying class item

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -3,13 +3,13 @@
 
 
 class Item:
-    def __init__(self,price, weight):
-        self.price = price      
+    def __init__(self,prix, weight):
+        self.price = prix     
         self.weight = weight  
 
     def display_item(self):
        
         print(f"Item: Price: {self.price}, Weight: {self.weight} kg")
 
-item1 = Item( 0.5, 10)  
+item1 = Item( 45, 10)  
 item1.display_item()                
